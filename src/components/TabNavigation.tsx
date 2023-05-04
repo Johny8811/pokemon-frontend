7efb6c80bbe95477bc3a659fe002f79b5f5e58c7
Jan Hujsa <janhujsa@JanPro-M1.local>
2023-05-04 14:54:38 +0200

    Add tab navigation component

---
+++ src/components/TabNavigation.tsx
@@ -0,0 +1,27 @@
+import './TabNavigation.css'
+
+export type ActiveTabState = 'all' | 'favorites'
+
+type Props = {
+    activeTab: ActiveTabState
+    setActiveTab: (param: ActiveTabState) => void
+}
+
+export const TabNavigation = ({ activeTab, setActiveTab }: Props) => {
+    return (
+        <div className="tab">
+            <button
+                className={activeTab === 'all' ? 'active' : ''}
+                onClick={() => setActiveTab('all')}
+            >
+                All
+            </button>
+            <button
+              className={activeTab === 'favorites' ? 'active' : ''}
+                onClick={() => setActiveTab('favorites')}
+            >
+                Favorites
+            </button>
+        </div>
+    )
+}
