f768d8ecbc6ea112e35343b77e6c3fef13ca51a7
Jan Hujsa <janhujsa@JanPro-M1.local>
2023-05-04 15:11:14 +0200

    Add favourite button component

---
+++ src/components/favouriteButton/FavouriteButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import heart from '../../assest/heart.png'
+import heartFilled from '../../assest/heart-filled.png'
+import './FavouriteButton.css'
+
+type Props = {
+    isFavourite: boolean,
+    onClick: () => void
+}
+
+export const FavouriteButton = ({ isFavourite, onClick }: Props) => {
+    const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        onClick();
+    }
+
+    return (
+        <button onClick={handleOnClick} className="favouriteButton">
+            {isFavourite ? (
+                <img
+                    src={heartFilled}
+                    alt="favourite"
+                />
+            ) : (
+                <img
+                    src={heart}
+                    alt="favourite"
+                />
+            )}
+        </button>
+    )
+}
