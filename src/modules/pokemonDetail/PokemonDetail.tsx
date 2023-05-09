0b0b7590394837105f08d27f84cd73dfc0a62637
Jan Hujsa <janhujsa@JanPro-M1.home>
2023-05-09 08:44:19 +0200

    Add pokemon detail content & show pokemon data to user

--- src/modules/pokemonDetail/PokemonDetail.tsx
+++ src/modules/pokemonDetail/PokemonDetail.tsx
@@ -1,5 +1,68 @@
+import { useParams } from 'react-router-dom'
+import { useLazyQuery } from '@apollo/client'
+import { useCallback, useEffect } from 'react'
+
+import { POKEMON_BY_ID } from '../../apollo/queries'
+import { FavouriteButton } from "../../components/favouriteButton/FavouriteButton";
+import './PokemonDetail.css'
+
 export const PokemonDetail = () => {
+  const { pokemonId } = useParams<{ pokemonId: string }>();
+  const [getPokemonById, { data, }] = useLazyQuery(POKEMON_BY_ID);
+
+  useEffect(() => {
+    if (pokemonId) {
+      void getPokemonById({
+        variables: {
+          id: pokemonId
+        }
+      })
+    }
+  }, [pokemonId])
+
+  const handleFavouritePokemon = useCallback((id: string) => async () => {
+    // TODO: add/remove from favourite
+  }, [])
+
+  const pokemonData = data?.pokemonById;
+
   return (
-    <div>Pokemon detail</div>
+    <main className="pokemonDetail">
+      {pokemonData && (
+        <div className="pokemonContent">
+          <img
+            src={pokemonData.image}
+            alt={pokemonData.name}
+            className="pokemonImage"
+          />
+          <div className="information">
+            <div className="description">
+              <span>
+                  <div>{pokemonData.name}</div>
+                  <div>{pokemonData.types}</div>
+              </span>
+              <FavouriteButton
+                isFavourite={pokemonData.isFavorite}
+                onClick={handleFavouritePokemon(pokemonData.id)}
+              />
+            </div>
+            <div className="power">
+              <div>CP: {pokemonData.maxCP}</div>
+              <div>HP: {pokemonData.maxHP}</div>
+            </div>
+            <div className="properties">
+              <div>
+                <div>Weight</div>
+                {pokemonData.weight.maximum} - {pokemonData.weight.minimum}
+              </div>
+              <div>
+                <div>Height</div>
+                {pokemonData.height.maximum} - {pokemonData.height.minimum}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </main>
   )
 }
