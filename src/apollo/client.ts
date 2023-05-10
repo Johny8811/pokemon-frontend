67fc638b08eaffc52a688e7db83f34165098175b
Jan Hujsa <janhujsa@JanPro-M1.home>
2023-05-10 18:54:08 +0200

    Add cache policy for pokemons field

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -3,6 +3,22 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const GraphQLEndpointUrl = 'http://localhost:4000/graphql'
 
 export const client = new ApolloClient({
-    uri: GraphQLEndpointUrl,
-    cache: new InMemoryCache(),
+  uri: GraphQLEndpointUrl,
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          pokemons: {
+            keyArgs: false,
+            merge: (existing = { edges: [] }, incoming) => ({
+              edges: [
+                ...existing.edges,
+                ...incoming.edges
+              ],
+            }),
+          }
+        }
+      }
+    }
+  }),
 });
