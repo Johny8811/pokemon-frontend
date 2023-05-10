49587ce09d6ded40b9a0667296eb5972ce4835cc
Jan Hujsa <janhujsa@JanPro-M1.home>
2023-05-10 08:27:35 +0200

    Add select component

---
+++ src/uiComponents/select/SelectInput.tsx
@@ -0,0 +1,21 @@
+import { ChangeEvent } from "react";
+
+import "./SelectInput.css"
+
+type Props = {
+  value: string,
+  items: string[],
+  defaultItem: string,
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void
+}
+
+export const SelectInput = ({ value, items, defaultItem, onChange }: Props) => {
+  return (
+    <select value={value} onChange={onChange} className="selectInput">
+      <option value="" selected>{defaultItem}</option>
+      {items.map(t => (
+        <option key={t} value={t}>{t}</option>
+      ))}
+    </select>
+  )
+}
