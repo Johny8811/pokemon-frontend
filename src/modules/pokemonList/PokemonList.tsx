b6b449966c5b96f11696ff8ec11a7576413bcac2
Jan Hujsa <janhujsa@JanPro-M1.home>
2023-05-10 12:09:43 +0200

    Clean code

--- src/modules/pokemonList/PokemonList.tsx
+++ src/modules/pokemonList/PokemonList.tsx
@@ -53,61 +53,62 @@ export const PokemonList = () => {
   }, [searchPokemon])
 
   return (
-      <div className="main">
-        <TabNavigation activeTab={tabNavigationState} setActiveTab={setTabNavigationState} />
-        <div className="pokemonsfilters">
-          <Search value={searchPokemon} onChange={setSearchPokemon} />
-          {pokemonTypesData?.pokemonTypes && (
-            <SelectInput
-              value={selectedPokemonType}
-              items={pokemonTypesData?.pokemonTypes}
-              defaultItem="All"
-              onChange={handleChangePokemonType}
-            />
-          )}
-          <div className="layout">
-            <Icon
-              icon={Icons.GRID}
-              alt="Grid"
-              dimension={30}
-              onClick={() => setLayout(Layot.GRID)}
-            />
-            <Icon
-              icon={Icons.LIST}
-              alt="List"
-              dimension={30}
-              onClick={() => setLayout(Layot.LIST)}
-            />
-          </div>
-        </div>
-        <div className={layout === Layot.GRID ? "grid" : "list"}>
-          {pokemonsData?.pokemons.edges.map(p => (
-            <div key={p.id} className="pokemonItem">
-              <Link to={`/pokemon/${p.id}`}>
-                <img
-                  src={p.image}
-                  alt={p.name}
-                  className="pokemonImage"
-                />
-                <div className="pokemonDescription">
-                  <span>
-                    <h3>
-                      {p.name}
-                    </h3>
-                    <p>
-                      {p.types.toString()}
-                    </p>
-                  </span>
-                  <FavouriteButton
-                    pokemonId={p.id}
-                    isFavorite={p.isFavorite}
-                    pokemonListQueryFilter={pokemonListQueryFilter}
-                  />
-                </div>
-              </Link>
-            </div>
-          ))}
+    <div className="main">
+      <TabNavigation activeTab={tabNavigationState} setActiveTab={setTabNavigationState} />
+      <div className="pokemonsfilters">
+        <Search value={searchPokemon} onChange={setSearchPokemon} />
+        {pokemonTypesData?.pokemonTypes && (
+          <SelectInput
+            value={selectedPokemonType}
+            // @ts-ignore
+            items={pokemonTypesData?.pokemonTypes}
+            defaultItem="All"
+            onChange={handleChangePokemonType}
+          />
+        )}
+        <div className="layout">
+          <Icon
+            icon={Icons.GRID}
+            alt="Grid"
+            dimension={30}
+            onClick={() => setLayout(Layot.GRID)}
+          />
+          <Icon
+            icon={Icons.LIST}
+            alt="List"
+            dimension={30}
+            onClick={() => setLayout(Layot.LIST)}
+          />
         </div>
       </div>
-    )
+      <div className={layout === Layot.GRID ? "grid" : "list"}>
+        {pokemonsData?.pokemons.edges.map(p => (
+          <div key={p.id} className="pokemonItem">
+            <Link to={`/pokemon/${p.id}`}>
+              <img
+                src={p.image}
+                alt={p.name}
+                className="pokemonImage"
+              />
+              <div className="pokemonDescription">
+                <span>
+                  <h3>
+                    {p.name}
+                  </h3>
+                  <p>
+                    {p.types.toString()}
+                  </p>
+                </span>
+                <FavouriteButton
+                  pokemonId={p.id}
+                  isFavorite={p.isFavorite}
+                  pokemonListQueryFilter={pokemonListQueryFilter}
+                />
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
